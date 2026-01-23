dbc21ef84fe517441c169a67e605846e1140b3ce
songhee <dlqnslthdgml@naver.com>
2026-01-23 13:09:07 +0900

    api 실습 강의

--- request_api/03_weather_city.py
+++ request_api/03_weather_city.py
@@ -7,4 +7,4 @@ api_key = '17a09334ee4793f86a0c7c9cc14fc7e2'
 url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
 
 response = requests.get(url).json()
-pprint(response)
+pprint(response)17a09334ee4793f86a0c7c9cc14fc7e2
