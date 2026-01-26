4b8f298f3561575d253154b3347c5a4ff0bfe37c
songhee <dlqnslthdgml@naver.com>
2026-01-26 15:19:36 +0900

    2026/01/26 강의코드 추가

---
+++ 강의/강의 코드/08_01_data_structure/02_sequence_method.py
@@ -0,0 +1,14 @@
+# index
+text = 'banana'
+print(text.index('a'))  # 1
+
+my_list = [1, 2, 3]
+print(my_list.index(2))  # 1
+
+
+# count
+text = 'banana'
+print(text.count('a'))  # 3
+
+my_list = [1, 2, 2, 3, 3, 3]
+print(my_list.count(3))  # 3
